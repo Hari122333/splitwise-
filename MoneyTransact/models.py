2b829bad25e46aa8eee7029ae9701cee43bce501
Priyank Pathak <priyankpathak50@gmail.com>
2017-03-24 18:35:49 +0530

    basic app

---
+++ MoneyTransact/models.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Balance(models.Model):
+    RUPEES = 'Rs'
+    DOLLAR = '$'
+    OTHERS = 'o/w'
+    Currency_CHOICES = (
+        (RUPEES, 'Rupees'),
+        (DOLLAR, 'Dollar'),
+        (OTHERS, 'Others'),
+    )
+    currency =models.CharField(max_length=3,
+        choices=Currency_CHOICES,
+        default=RUPEES,
+    )
+    Giver = models.CharField(max_length=50)
+    taker = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    reason = models.CharField(max_length=250)
+    place = models.CharField(max_length=50)
+
+
+
